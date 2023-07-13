400ce79d28d3d4b5ae7ea112d326b8ad0500ebd3
mahabradai <br.maha.coder@gmail.com>
2023-07-13 16:41:18 +0200

    first commit

---
+++ gateway/src/main/java/com/brcoder/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.brcoder.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
